b9a81d97282a09a7a4f73fb4089fa748fbc4588c
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 17:51:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
 
-def roll_call_dwarves(array)
+def roll_call_dwarves(dwarves)
   i = 0 
   while i < array.length 
     puts "#{i + 1}. #{array[i]}"
     i += 1 
   end 
-  roll_call_dwarves(array)
+  roll_call_dwarves(dwarves)
 end
 
 
