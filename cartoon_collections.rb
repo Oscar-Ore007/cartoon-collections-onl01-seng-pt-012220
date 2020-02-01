63b384ab57b9d9e33c2b2a19bcd085662478f9f6
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-01 17:59:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ roll_call_dwarves(dwarves)
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(array)
-  planeteer_calls.map! { |i|
+  planeteer_calls.map! { |name| 
 end
 
 def long_planeteer_calls# code an argument here
